3cdc3403d17c225a7d2bd9f6eeaba85fa21b788c
zhouxiao <zhouxiao@s4302-PowerEdge-R410.(none)>
2012-05-28 14:37:44 +0800

    xx

---
+++ java2/BeerSong.java
@@ -0,0 +1,14 @@
+public class BeerSong {
+	
+	public static void main (String[] args) {
+		int beerNum = 99;
+		String word = "bottles";
+
+		while (beerNum > 0) {			
+
+		System.out.println("hello world");
+
+		beerNum -= 1;		
+		}
+	}
+}
